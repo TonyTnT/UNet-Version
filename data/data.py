2045939d5afe76b400720a1ab1432332d7bf1589
41799 <chenx2ovo@gmail.com>
2020-09-08 00:21:31 +0800

    读取图片的载入方式修改 PIL-》opencv

--- data/data.py
+++ data/data.py
@@ -18,16 +18,12 @@ class ImgSeg(data.Dataset):
         self.filenames = os.listdir(self.jpg_path)
 
     def __getitem__(self, index):
-        # img = Image.open(os.path.join(self.jpg_path, self.filenames[index])).convert('RGB')
-        # seg = Image.open(os.path.join(self.seg_path, self.filenames[index].replace('.jpg', '_mask.jpg'))).convert('1')
         img = cv2.imread(os.path.join(self.jpg_path, self.filenames[index]))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         seg = cv2.imread(os.path.join(self.seg_path, self.filenames[index].replace('.jpg', '_mask.jpg')), 0)
 
         if self.trans is not None:
             aug= self.trans(image=img, mask=seg)
-
-        # print(aug["image"].shape, aug["mask"].shape)
 
         return aug["image"], aug["mask"]
 
